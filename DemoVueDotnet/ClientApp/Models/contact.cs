4d2146a082f09a94636eb1ebcbb32fc53cd2ac35
Taishan-web <66307952+Taishan-web@users.noreply.github.com>
2020-10-28 10:27:56 +0800

    Create contact.cs

---
+++ DemoVueDotnet/ClientApp/Models/contact.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoVueDotnet.ClientApp.Models
+{
+    public class Contact
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNo { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
